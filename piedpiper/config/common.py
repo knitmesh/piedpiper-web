0f73584ec3ff6988a74472f43ff1510618cfd2c9
lijinghui <lijinghui@t2cloud.net>
2018-04-19 18:00:27 +0800

    ci(travis): add Heroku credentials

--- piedpiper/config/common.py
+++ piedpiper/config/common.py
@@ -72,7 +72,9 @@ class Common(Configuration):
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/2.0/howto/static-files/
     STATIC_ROOT = os.path.normpath(join(os.path.dirname(BASE_DIR), 'static'))
-    STATICFILES_DIRS = []
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
     STATIC_URL = '/static/'
     STATICFILES_FINDERS = (
         'django.contrib.staticfiles.finders.FileSystemFinder',
